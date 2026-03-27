4605bd0f8ddd36703b41ff6c2a0f6f9ee828b886
Mary <218518147+m-52090575@users.noreply.github.com>
2026-03-27 16:53:35 -0700

    replace the li text content only, not the whole node

--- src/index.js
+++ src/index.js
@@ -27,8 +27,7 @@ function editTodo(event) {
   let oldTodo = event.target.parentNode.children[0].textContent;
   let newTodo = prompt("Edit Todo", oldTodo);
   if (newTodo !== "" && newTodo != null) {
-    let li = createLi(newTodo);
-    event.target.parentNode.replaceWith(li);
+    event.target.parentNode.children[0].textContent = newTodo;
   }
 }
 
@@ -36,7 +35,22 @@ main.addEventListener("submit", (event) => {
   event.preventDefault();
   let formInput = main.getElementsByTagName('input')[0].value;
   if (formInput !== "") {
-    let li = createLi(formInput);
+    let li = document.createElement("li");
+
+    let span = document.createElement("span");
+    span.textContent = formInput;
+
+    let editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.addEventListener("click", editTodo);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("deletebtn");
+    deleteButton.addEventListener("click", deleteParent);
+    li.append(span, editButton, deleteButton);
+
+
     taskList.append(li);
 
     main.getElementsByTagName('input')[0].value = "";
