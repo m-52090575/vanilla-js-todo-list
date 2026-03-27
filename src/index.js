13a608c942622f716ba6b1c79c8434cbe72f0be0
Mary <218518147+m-52090575@users.noreply.github.com>
2026-03-26 20:33:54 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+var main = document.getElementById("main");
+//var submitbtn = document.getElementById("submitbtn");
+var taskList = document.getElementById("task-list");
+
+main.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let formInput = main.getElementsByTagName('input')[0].value;
+  if (formInput !== "") {
+    let li = document.createElement("li");
+
+    let span = document.createElement("span");
+    span.textContent = formInput;
+
+    let button = document.createElement("button");
+    button.textContent = "Delete";
+    button.classList.add("deletebtn");
+    button.addEventListener("click", deleteParent);
+    li.append(span, button);
+
+
+    taskList.append(li);
+
+    main.getElementsByTagName('input')[0].value = "";
+  }
+});
+
+var deletebtns = main.getElementsByClassName("deletebtn");
+for (var i = 0; i < deletebtns.length; i++) {
+  deletebtns[i].addEventListener("click", deleteParent);
+}
+
+function deleteParent(event) {
+  event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+  // removes the delete button
+  //event.target.parentNode.removeChild(event.target);
+  //console.log("delete clicked");
+  //console.log(event);
+}
